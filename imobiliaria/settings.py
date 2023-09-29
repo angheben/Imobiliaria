b6f51a5c3bf4008d34a8af61dc777a20e7d6de32
Vitor Angheben <vitor_angheben@hotmail.com>
2023-09-29 09:57:09 -0300

    Prepared to vercel deployment

--- imobiliaria/settings.py
+++ imobiliaria/settings.py
@@ -123,9 +123,6 @@ MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
-
-# Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
